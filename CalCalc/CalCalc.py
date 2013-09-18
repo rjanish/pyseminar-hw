c38208035c7f3bfde29b81b8f0ce7ff76799d7d1
Ryan Janish <ryan.janish@gmail.com>
2013-09-18 15:31:53 -0700

    added directory to hold CalCalc module

--- CalCalc/CalCalc.py
+++ CalCalc/CalCalc.py
@@ -81,6 +81,23 @@ def calculate(target, force_wolfram=False):
 
 ########################################################################
 
+def test_1(): 
+	assert calculate("3 + 7") == 11
+
+def test_2():
+	assert abs(calculate("5.0**3.0 - 125.0")) < 10**(-12)
+
+def test_3():
+	assert abs(calculate("Mass of the proton in grams") - 1.672622*10**(-24)) < 10**(-5)
+
+def test_4():
+	assert "42" in calculate("What is the meaning of life?")
+
+def test_5():
+	assert abs(calculate("e^(sqrt(-1)*2pi)") - 1.0) < 10**(-12)
+
+########################################################################	
+
 if __name__ == '__main__':
 	# process cmd line args
 	parser = argparse.ArgumentParser("Generate a histogram")
---
+++ CalCalc/README.txt
@@ -0,0 +1,9 @@
+This is a module for evaluating strings either locally in python or by sending the string to wolfram alpha.  
+
+Evaluation is implemented in the calculate function in CalCalc.  Basic usage is, from python:
+	> from CalCalc import calculate
+	> calculate("string to evaluate")
+or from the command line:
+	$ python CalCalc.py "string to evaluate"
+For more details and options, see the calculate docstring or from the command line:
+	$ python CalCalc.py --help
