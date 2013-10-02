5fde366132bed07192d9ffb76a55409936920c48
Ryan Janish <ryan.janish@gmail.com>
2013-10-02 12:58:10 -0700

    added function to run classifier on directory of uncategorized images, edited to use string classifier lables instead of ints'

--- hw4/image_classifier.py
+++ hw4/image_classifier.py
@@ -1,10 +1,13 @@
 
 import os
+import sys
+import pickle
 
 import numpy as np
+from numpy.random import shuffle as npshuffle
 from scipy.ndimage import imread
 from sklearn.ensemble import RandomForestClassifier
-from sklearn import grid_search
+from sklearn import cross_validation as cval
 
 class ImageFeaturizer(object):
     '''
@@ -48,24 +51,35 @@ class ImageFeaturizer(object):
     def get_features(self):
         return np.array(self.features)
 
+    def feature_names(self):
+        return [feature_func.func_name for feature_func 
+                                       in self.features_to_use]
+
     # begin feature calculators
     def red_fraction(self, image):
-        return image[:,:,0].sum()/float(image.sum())
+        if len(image.shape) == 3:
+            return image[:,:,0].sum()/float(image.sum())
+        else:
+            return 0.0
 
     def blue_fraction(self, image):
-        return image[:,:,1].sum()/float(image.sum())
+        if len(image.shape) == 3:
+            return image[:,:,1].sum()/float(image.sum())
+        else:
+            return 0.0
 
     def green_fraction(self, image):
-        return image[:,:,2].sum()/float(image.sum())
+        if len(image.shape) == 3:
+            return image[:,:,2].sum()/float(image.sum())
+        else:
+            return 0.0
 
 
 def prepare_training_set(training_dir):
     '''
     Gather filenames and category names from a training set, assigns each
-    category an integer label and returns a list of training image filenames,
-    a corresponding list of the category label of each image, and a list of
-    category string names indexed by integer label. 
-
+    category a label and returns a list of training image filenames and
+    a corresponding list of the category labels of each image.
 
     Arguments:
     training_dir - directory that holds training set.  The structure is 
@@ -85,25 +99,96 @@ def prepare_training_set(training_dir):
     corresponding category.  
 
     Output:
-    a tuple (image_files, categories, cat_names)
+    a tuple (image_files, categories)
     image_files - list of all training image filenames
     categories - list of the category of each training image, respectively
-    cat_name - list of string names for each category, such that the name 
-    of the nth category is given by cat_name[n]
     '''
+    if training_dir[-1] == '/':
+        training_dir = training_dir[:-1]
     cat_names = [d for d in os.listdir(training_dir)
                     if os.path.isdir(training_dir + '/' + d)]
     cat_dirs = [training_dir + '/' + catagory for catagory in cat_names]
-    catagories = []
+    categories = []
     image_files = []
-    for cat_num, (cat, cat_dir) in enumerate(zip(cat_names, cat_dirs)):
+    for cat_name, cat_dir in zip(cat_names, cat_dirs):
         images = [cat_dir + '/' + f for f in os.listdir(cat_dir) 
                                     if (os.path.isfile(cat_dir + '/' + f) and
                                         f[0] != '.')]
-        catagories += [cat_num]*len(images)
+        categories += [cat_name]*len(images)
         image_files += images
-    return image_files, np.array(catagories), cat_names
+    return image_files, np.array(categories)
 
-def construct_classifier(training_dir):
-    image_files, catagories, cat_names = prepare_training_set(training_dir)
-    feautres = ImageFeaturizer(images=image_files).get_features()
+def construct_classifier(training_dir, folds=20, 
+                         classifier_file='trained_classifier.p'):
+    '''
+    Construct a random forest image classifier from the training data in 
+    the passed directory.  The directory must be formated as described 
+    in the function "prepare_training_set".  The final classifier will be 
+    pickled as well as returned.
+
+    Arguments:
+    training_dir - directory of training data, for formating specifications
+    see "prepare_training_set"
+    folds - number of folds of cross_validation to preform, default is 20
+    classifier_file - file destination for pickled final classifier, default
+    is 'trained_classifier.p'
+
+    Output:
+    tuple (clf, accuracy)
+    clf - final classifier object
+    accuracy - median accuracy of classifier via cross validation
+    '''
+    # get data, compute features
+    image_files, categories = prepare_training_set(training_dir)
+    featurizer = ImageFeaturizer(images=image_files)
+    features = featurizer.get_features()
+    feature_names = np.array(featurizer.feature_names())
+    # shuffle and estimate accuracy with cross validation
+    shuffle_index = np.arange(features.shape[0])
+    npshuffle(shuffle_index)
+    features = features[shuffle_index, :]
+    categories = categories[shuffle_index, :]
+    cv_splits = cval.KFold(len(categories), folds, shuffle=True)
+    cv_scores = cval.cross_val_score(RandomForestClassifier(), features, 
+                                     categories, cv=cv_splits, n_jobs=-1)
+    accuracy = np.median(cv_scores)
+    random_guessing = 1.0/len(np.unique(categories))
+    # build classifier on full training set
+    full_classifier = RandomForestClassifier(n_estimators=50, n_jobs=-1, 
+                                             compute_importances=True)
+    full_classifier.fit(features, categories)
+    # output stats and save full classifier
+    pickle.dump(full_classifier, open(classifier_file,'w'))
+    print ("\nbuilt random forest classifier, "
+           "saved to: {}".format(classifier_file))
+    print "model accuracy: {:.3f}".format(accuracy)
+    print "accuracy from random guessing: {:.3f}".format(random_guessing)
+    improv_factor = (accuracy - random_guessing)/float(random_guessing)
+    print ("improvement factor over random guessing: "
+           "{:.1f}".format(improv_factor))
+    importances = np.argsort(full_classifier.feature_importances_)
+    sorted_features = feature_names[importances]
+    print "three most important features:"
+    for feature_name in sorted_features[:3]:
+        print "\t{}".format(feature_name)
+    return full_classifier, accuracy
+
+def run_final_classifier(path, forest='trained_classifier.p', 
+                         print_results=True):
+    clf = pickle.load(open(forest, "rb"))
+    if path[-1] == '/':
+        path = path[:-1]
+    images = [path + '/' + f for f in os.listdir(path) 
+                             if (os.path.isfile(path + '/' + f) and
+                                 f[0] != '.')]
+    featurizer = ImageFeaturizer(images=images)
+    features = featurizer.get_features()
+    feature_names = np.array(featurizer.feature_names())
+    results = clf.predict(features)
+    print "filename\t\t\tcategory"
+    print "------------------------------------------"
+    for im, cat in zip(images, results):
+        print "{}\t\t{}".format(im, cat)
+    return
+
+#############################################################
