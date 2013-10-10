1c1e333c473f95c01a904495153e93191c238529
Ryan Janish <ryan.janish@gmail.com>
2013-10-10 04:36:40 -0500

    added image method docstring, made image methods return lists

--- hw5/image-server.py
+++ hw5/image-server.py
@@ -5,7 +5,13 @@ import numpy as np
 from scipy.fftpack import fft2, ifft2, fftshift
 
 class ImageMethods(object):
+    """ A bucket of image manipulation routines """
+
     def invert(self, image):
+        """
+        Invert the brightness of an image.  For RGB images each color 
+        channel is inverted separately.
+        """
         image = np.array(image)
         if len(image.shape) == 3:
             maxes = np.max(np.max(image, axis=0), axis=0)
@@ -16,9 +22,14 @@ class ImageMethods(object):
             image = max_gray - image
         else:
             raise Exception("Invalid image shape: {}".foramt(image.shape))
-        return image
+        return image.tolist()
 
     def frequency_space_by_channel(self, image): 
+        """ 
+        Compute the Fourier transform of the image, treating each color 
+        channel separately if the image is in RGB form.  Output is centered 
+        such that the zero frequency mode is in the center.
+        """
         image = np.array(image)
         if len(image.shape) == 3:
             freq_space = np.zeros(image.shape)
@@ -28,9 +39,15 @@ class ImageMethods(object):
             freq_space = fftshift(fft2(image))
         else:
             raise Exception("Invalid image shape: {}".foramt(image.shape))
-        return freq_space
+        return freq_space.tolist()
 
     def colorize_by_power(self, image):
+        """
+        Colorize the image mode-by-mode according to the power in each mode.  
+        The top third of modes are colored red, the middle third green, and 
+        the lower third blue.  For RGB images, a grayscale equivalent is 
+        computed and colorized. 
+        """
         image = np.array(image)
         if len(image.shape) == 3:
             power = fft2(np.sum(image, axis=2))**2
@@ -51,14 +68,14 @@ class ImageMethods(object):
             shifted = (new_channel - new_channel.min())
             scaled = 255.0*shifted/shifted.max()
             colorized[..., color] = ifft2(np.where(region, power, 0.0))
-        return colorized
+        return colorized.tolist()
 
 class ImageServer(object):
     __init__(self, name='localhost', port=5000, help=True):
         self.name = name
         self.port = port
 
-    def run():
+    def run(self):
         server = SimpleXMLRPCServer((name, port))
         server.register_instance(ImageMethods())
         print "Starting ImageServer........"
@@ -67,6 +84,7 @@ class ImageServer(object):
             server.serve_forever()
         except KeyboardInterrupt:
             print "ImageServer exiting........."
+
             
 if __name__ == '__main__':
     im_server = ImageServer()
