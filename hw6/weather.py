35965aaedaa7e6013c1081e5fce1f24966007e50
Ryan Janish <ryan.janish@gmail.com>
2013-10-16 22:56:08 -0700

    removed numpy loadtxt, now uses csv module which is much easier. makes and inserts data into a database for each data file

--- hw6/weather.py
+++ hw6/weather.py
@@ -1,50 +1,41 @@
 
+import csv
+
 import numpy as np
 import sqlalchemy as sql 
 
-def max_entry_size(filename):
-    '''Compute the maximum character length of an entry in a csv file'''
-    file_max = 0
-    with open(filename, 'r') as csv_file:
-        for line in csv_file.readlines():
-            line = line[:-1] # remove newline
-            line_max = max([len(entry) for entry in line.split(",")])
-            if line_max > file_max:
-                file_max = line_max
-    return  file_max
 
-# load all airport and top 50 airport data 
-# The use of '^' as comments is to be sure that no characters are 
-# flagged as comments, as there are no ^s in the data files.
-# The choice of 190 and 70 for string size limits were determined by 
-# running the above function 'max_entry_size' on the two datafiles
-all_airports = np.loadtxt("ICAO_airports.csv", delimiter=',',
-                          skiprows=1, comments='^',
-                          dtype=[("id", int),
-                                 ("ident", 'S190'),
-                                 ("type", 'S190'),
-                                 ("name", 'S190'),
-                                 ("latitude_deg", 'S190'),
-                                 ("longitude_deg", 'S190'),
-                                 ("elevation_ft", 'S190'),
-                                 ("continent", 'S190'),
-                                 ("iso_country", 'S190'),
-                                 ("iso_region", 'S190'),
-                                 ("municipality", 'S190'),
-                                 ("scheduled_service", 'S190'),
-                                 ("gps_code", 'S190'),
-                                 ("iata_code", 'S190'),
-                                 ("local_code", 'S190'),
-                                 ("home_link", 'S190'),
-                                 ("wikipedia_link", 'S190'),
-                                 ("keywords", 'S190')]) 
-top_airports = np.loadtxt("top_airports.csv", delimiter=',',
-                          skiprows=1, comments='^',
-                          dtype=[("City", 'S70'),
-                                 ("FAA", 'S70'),
-                                 ("IATA", 'S70'),
-                                 ("ICAO", 'S70'),
-                                 ("Airport", 'S70'),
-                                 ("Role", 'S70'),
-                                 ("Enplanements", int)])
+# read all airport data,
+# save name, icao code, lat, and long 
+all_airports = []
+all_airports_file = csv.reader(open("ICAO_airports.csv", 'r'))
+all_airports_file.next() # skip header
+for row in all_airports_file:
+    all_airports.append({"icao": unicode(row[1], 'utf-8'), # sql needs to be
+                         "name": unicode(row[3], 'utf-8'), # given unicode
+                         "lat": float(row[4]), 
+                         "long": float(row[5])})
+# read top airport data,
+# save city and icao codes
+top_airports = []
+top_airports_file = csv.reader(open("top_airports.csv", 'r'))
+top_airports_file.next() # skip header
+for row in top_airports_file:
+    top_airports.append({"city": unicode(row[0], 'utf-8'),
+                         "icao": unicode(row[3], 'utf-8')})
 
+# make databases
+engine = sql.create_engine('sqlite:///:memory:')
+metadata = sql.MetaData()
+all_airports_table = sql.Table('all_airports', metadata,
+                               sql.Column('icao', sql.String),
+                               sql.Column('name', sql.String),
+                               sql.Column('lat', sql.Float),
+                               sql.Column('long', sql.Float))
+top_airports_table = sql.Table('top_airports', metadata,
+                               sql.Column('city', sql.String),
+                               sql.Column('icao', sql.String))
+all_airports_table.create(bind=engine)
+top_airports_table.create(bind=engine)
+engine.execute(all_airports_table.insert(), all_airports).rowcount
+engine.execute(top_airports_table.insert(), list(top_airports)).rowcount
