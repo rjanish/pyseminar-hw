a1dc85a06357d8ec067f81e5eb3d227e8c5a49e2
Ryan Janish <ryan.janish@gmail.com>
2013-11-06 16:17:12 -0800

    added numpy vectorized method, seperated loop method into a loop function and a pi-scaling function

--- hw8/parallel_pi.py
+++ hw8/parallel_pi.py
@@ -1,5 +1,18 @@
 
+import numpy as np
 from numpy.random import random
+
+def count_hits(n):
+	''' 
+	Simulate n dart throws into the first quadrant of [-1,1]x[-1,1] 
+	and count the number of throws inside the unit circle
+	'''
+	hits = 0
+	for throw in xrange(int(n)):
+		x, y = random(), random()
+		if x**2 + y**2 <= 1.0:	  
+			hits += 1 
+	return hits			  
 
 def compute_pi_pythonloop(n):
 	'''
@@ -7,9 +20,16 @@ def compute_pi_pythonloop(n):
 	pure python loops.  n is the number of darts simulated.
 	'''
 	hits = 0
-	for throw in xrange(int(n)):
-		x, y = random(), random() # get point in quadrant I of [-1,1]x[-1,1]
-		if x**2 + y**2 <= 1.0:	  
-			hits += 1 			  # count if point is inside unit circle
+	pi_approx = 4*count_hits(n)/float(n)
+	return pi_approx
+
+def compute_pi_numpy(n):
+	'''
+	Approximate pi using a monte-carlo dartboard, implemented with 
+	numpy vectorization.  n is the number of darts simulated.
+	'''
+	x, y = random(n), random(n) # n points in quadrant I of [-1,1]x[-1,1]
+	rsq = x**2 + y**2
+	hits = np.sum(rsq <= 1)		# count number of points inside unit circle
 	pi_approx = 4*hits/float(n)
 	return pi_approx
