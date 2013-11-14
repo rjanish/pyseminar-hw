84901aeaac662c42fb6efbcbc75a79f80af77241
Ryan Janish <ryan.janish@gmail.com>
2013-11-13 16:03:52 -0800

    added tracking and listing of collection names

--- hw10/bib_page.py
+++ hw10/bib_page.py
@@ -1,18 +1,27 @@
 from flask import Flask, render_template, request, url_for
+
 app = Flask(__name__)
 app.debug = True
 
+collections = []
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
-	if request.method == 'POST':
-		if 'query' in request.form:
-			return render_template('index.html', 
-								   searched=request.form['query'])
-		if 'filename' in request.form:
-			return render_template('index.html',
-								   uploaded=request.form['filename'])
-	else:
-		return render_template('index.html')
+    if request.method == 'POST':
+        if 'collection_name' in request.form:
+            collections.append(request.form['collection_name'])
+            # upload file
+            return render_template('index.html', 
+                                   collections=collections)
+        if 'query' in request.form:
+            return render_template('index.html', 
+                                   collections=collections)
+        else:
+            return render_template('index.html', 
+                                   collections=collections)
+    else:
+        return render_template('index.html', 
+                               collections=collections)
 
 
 if __name__ == "__main__":
